5a35150234855eb7fb91cd52feb659e4817b9006
Neblslaw <mateusz.gielczynski@outlook.com>
2023-04-11 10:40:04 +0200

    działą zamiana dwóch liczb ze stringa na u2

--- task2/arch2.asm
+++ task2/arch2.asm
@@ -20,29 +20,75 @@ Kod             SEGMENT
 Start:
 
 
-                jmp st
+                jmp startt
 wynik           DW      0h
 
 a               DB       "17" 
 b               DB      "-11"
-an              DB      0
+an              DW      0
+znak            DB      0
 
 
-st:
+startt:
+        mov     si,OFFSET a   
+        cmp     [si] , '-'      
+        je      minus1
 
-        mov     si,OFFSET a         
 looop:  cmp     si,OFFSET b
-        je      Koniec
-        mov     bx,10
-        mul     bx
-        mov     bx,[si]
-        sub     bx , 48
+        je      next
+        mov     bl,10
+        mul     bl
+        mov     bl,[si]
+        sub     bl , 48
         add     al,bl
         inc     si
         jmp     looop
+minus1:
+        mov     znak,1
+        inc     si
+        jmp     looop
+
+next:
+        cmp     znak,1
+        je      ujemna
+        jne     nexxt
+ujemna: 
+        not     ax
+        inc     ax
+        jmp     nexxt
+
+nexxt:
+        cmp     an,0          ;obie liczby powinny być zmienione
+        jne     dodaj
+        mov     an, ax        ; w an 11h = 17d czyli działa tlumaczenie stringa a na inta  
+        mov     ax, 0
+        mov     znak,0
+
+        mov     si,OFFSET b   
+        cmp     byte ptr [si] , '-'     ;byte ptr zeby porównać 8 bitów nie 16 
+        je      minus2
+looop2:
+        cmp     si,OFFSET an
+        je      next
+        mov     bl,10
+        mul     bl
+        mov     bl,[si]
+        sub     bl , 48
+        add     al,bl
+        inc     si
+        jmp     looop2
+minus2:
+        mov     znak,1
+        inc     si
+        jmp     looop2
+
+;koniec zamiany liczb
+
+dodaj:
+        ;kod na dodawanie trzeba by dodać
+        ;an + ax
 
 Koniec:        
-                mov     wynik, ax        ; w wyniku 11h = 17d czyli działa tlumaczenie stringa a na inta  
                 mov     ax, 4C00h               ;4C mowi systemowi ze konczy dzialanie programu i zwraca wartosc ah jako kod bledu 00 w tym przypadku
                 int     21h                     ;przerwanie systemowe konczace program
 
