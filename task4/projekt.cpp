08a09feb0d9ec1fdc97ffe4c23a8a56865407984
Neblslaw <mateusz.gielczynski@outlook.com>
2023-05-31 11:30:18 +0200

    dziala

--- task4/projekt.cpp
+++ task4/projekt.cpp
@@ -1,133 +1,208 @@
-//======================================================================================//
-//                                                                            			//
-// Plik           : Archi4                                    							//
-// Format         : EXE                                                        			//
-// Cwiczenie      : Tryb graficzny                                          			//
-// Autorzy        : Gabriela Szkiłondź, Paulina Papiernik, gr 3, poniedzialek, 14:15	//
-// Data zaliczenia: DD.MM.ROK						   									//
-//                                                                             			//
-//======================================================================================//
+#include <dos.h>            //intr, regpack
+#include <iostream.h>       //input/output
+#include <conio.h>          //pliki
+#include <stdio.h>          //getch, clrscr
 
-#include <dos.h>
-#include <iostream.h>
-#include <stdio.h>
-#include <conio. h>
+typedef unsigned char BYTE;
+typedef unsigned int WORD;
+typedef unsigned int UINT;
+typedef unsigned long DWORD;
+typedef unsigned long LONG; 
 
-	typedef unsigned char BYTE;
-	typedef unsigned int WORD;
-	typedef unsigned int UINT;
-	typedef unsigned long DWORD;
-	typedef unsigned long LONG; 
- 
-	struct BITMAPFILEHEADER
-	{
-		UINT bfType; //Opis formatu pliku. Musi być ‘BM’.
-		DWORD bfSize; //Rozmiar pliku BMP w bajtach.
-		UINT bfReserved1; //Zarezerwowane. Musi być równe 0.
-		UINT bfReserved2; //Zarezerwowane. Musi być równe 0.
-		DWORD bfOffBits; //Przesunięcie w bajtach początku danych
-	}; 
+struct BITMAPFILEHEADER{
+    UINT bfType; //Opis formatu pliku. Musi być ‘BM’.
+    DWORD bfSize; //Rozmiar pliku BMP w bajtach.
+    UINT bfReserved1; //Zarezerwowane. Musi być równe 0.
+    UINT bfReserved2; //Zarezerwowane. Musi być równe 0.
+    DWORD bfOffBits; //Przesunięcie w bajtach początku danych
+}; 
 	
-	struct BITMAPINFOHEADER
-	 {
-		 DWORD biSize; //Rozmiar struktury BITMAPINFOHEADER.
-		 LONG biWidth; //Szerokość bitmapy w pikselach.
-		 LONG biHeight; //Wysokość bitmapy w pikselach.
-		 WORD biPlanes; //Ilość płaszczyzn. Musi być 1.
-		 WORD biBitCount; //Głębia kolorów w bitach na piksel.
-		 DWORD biCompression; //Rodzaj kompresji (0 – brak).
-		 DWORD biSizeImage; //Rozmiar obrazu w bajtach. Uwaga może być 0.
-		 LONG biXPelsPerMeter;//Rozdzielczość pozioma w pikselach na metr.
-		 LONG biYPelsPerMeter;//Rozdzielczość pionowa w pikselach na metr.
-		 DWORD biClrUsed; //Ilość używanych kolorów z palety.
-		 DWORD biClrImportant; //Ilość kolorów z palety niezbędnych do
-	 }; //wyświetlenia obrazu. 
+struct BITMAPINFOHEADER{
+    DWORD biSize; //Rozmiar struktury BITMAPINFOHEADER.
+    LONG biWidth; //Szerokość bitmapy w pikselach.
+    LONG biHeight; //Wysokość bitmapy w pikselach.
+    WORD biPlanes; //Ilość płaszczyzn. Musi być 1.
+    WORD biBitCount; //Głębia kolorów w bitach na piksel.
+    DWORD biCompression; //Rodzaj kompresji (0 – brak).
+    DWORD biSizeImage; //Rozmiar obrazu w bajtach. Uwaga może być 0.
+    LONG biXPelsPerMeter;//Rozdzielczość pozioma w pikselach na metr.
+    LONG biYPelsPerMeter;//Rozdzielczość pionowa w pikselach na metr.
+    DWORD biClrUsed; //Ilość używanych kolorów z palety.
+    DWORD biClrImportant; //Ilość kolorów z palety niezbędnych do wyświetlenia obrazu. 
+}; 
 
- struct RGBQUAD 
- { 
- BYTE rgbBlue; 
- BYTE rgbGreen; 
- BYTE rgbRed; 
- BYTE rgbReserved; 
- }; 
+struct RGBQUAD { 
+    BYTE rgbBlue; 
+    BYTE rgbGreen; 
+    BYTE rgbRed; 
+    BYTE rgbReserved; 
+};
 
-	
 
+
+int kolor = 0;          //bool 1-sa kolory 0-nie ma
+
+char fileName[6][20]={"IMAGES/AREA.bmp","IMAGES/BABOON.bmp","IMAGES/LENA.bmp","IMAGES/PEPPERS.bmp","IMAGES/PLANE.bmp","IMAGES/TANK.bmp"};
+int fileNumber;
+BITMAPFILEHEADER BM_fileHeader;
+BITMAPINFOHEADER BM_infoHeader;	
+RGBQUAD paleta[256] ; 
+unsigned char far* video_memory = (BYTE *)0xA0000000L;			//nie wiem dlaczego to far ale inaczej jest null pointer
+
+
+
+void trybTekstowy(){
+    REGPACK regs;					
+    regs.r_ax = 0x10;
+    intr(0x10, &regs);              //int 16  z rejestrami ze struktury regs
+}	
+void trybGraficzny(){
+    REGPACK regs;					
+    regs.r_ax = 0x13;
+    intr(0x10, &regs);
+}
+
+void ustawPaleteKolorow(int flaga){            //bool nie istnieje
+    outportb(0x03C8, 0); 				//rozpocznij ustawianie palety od koloru nr 0
+    for (int i = 0; i < 256; i++) 		    //ilość kolorów w palecie 8-bitowej
+    {
+        outp(0x03C9, (flaga ? i:(int)paleta[i].rgbRed)   * 63 / 255); 	//skalowana składowa R
+        outp(0x03C9, (flaga ? i:(int)paleta[i].rgbGreen) * 63 / 255); 	//skalowana składowa G
+        outp(0x03C9, (flaga ? i:(int)paleta[i].rgbBlue)  * 63 / 255); 	//skalowana składowa B
+    }
+}
+
+void clrscr(){              //nadpisanie tej funkcji bo standardowa zostawia szary ekran a nie czarny
+    trybGraficzny();
+        for(int i=199; i>=0; i--){											
+        for(int j=0; j<320; j++){
+            video_memory[320*i + j] = 0;		
+        }																
+    }	
+    trybTekstowy();
+}
+
+void wyswietl(){
+    FILE * file = fopen(fileName[fileNumber], "rb");
+    trybGraficzny();
+
+    fread(&BM_fileHeader, sizeof(BITMAPFILEHEADER), 1, file);
+    fread(&BM_infoHeader, sizeof(BITMAPINFOHEADER), 1, file); 
+
+    fread(&paleta, sizeof(RGBQUAD),256,file);
     
+  //  fseek(file,BM_fileHeader.bfOffBits,SEEK_SET);  				// Od poczatku pliku przesuwa o ten offset dzieki czemu jesteśmy w miejscu gdzie zaczynaja sie konkretne piksele obrazu
+            //powyższa linia pomija palete kolorów ale że my używamy kolorków to można ją usunąć
 
-	unsigned char far* video_memory = (BYTE *)0xA0000000L;			//Ogolnie to musi byc far, far away, ale kto by sie tym przejmowal...
-																	//Patrz: Dalekie wskazniki
-	
-	//Uczcie i czerpcie z tego wszyscy...
-	int main(){
+    for(int i=199; i>=0; i--){											//obraz jest od tyłu
+        for(int j=0; j<320; j++){
+            video_memory[320*i + j] = fgetc(file); 		
+        }																
+    }
+    ustawPaleteKolorow(kolor);
+    fclose(file);
+    getch();
+    trybTekstowy();	
+}
 
+void wybierzPlik(){
+    clrscr();
+    cout<<"Wybierz:\n\
+    0.AREA\n\
+    1.BABOON\n\
+    2.LENA\n\
+    3.PEPPERS\n\
+    4.PLANE\n\
+    5.TANK\n";
+    cin>>fileNumber;
+}
 
-        //here tryb graficzny:
-		REGPACK regs;					
-		regs.r_ax = 0x13;
-		intr(0x10, &regs);
+void progowanie(){
+    cout<<"podaj prog: ";
+    int prog;
+    cin>>prog;
+    clrscr();
+    trybGraficzny();
+    FILE * file = fopen(fileName[fileNumber], "rb");
+    fread(&BM_fileHeader, sizeof(BITMAPFILEHEADER), 1, file);
+    fread(&BM_infoHeader, sizeof(BITMAPINFOHEADER), 1, file); 
 
+    fread(&paleta, sizeof(RGBQUAD),256,file);
 
-		//here czytanie danych o pliku:
-		FILE * file = fopen("IMAGES/BABOON.bmp", "rb");
-		
-        BITMAPFILEHEADER BM_fileHeader; //nagłówek nr 1 bitmapy
-	    BITMAPINFOHEADER BM_infoHeader; //nagłówek nr 2 bitmapy
+    int wartosc;
+    for(int i=199; i>=0; i--){											//obraz jest od tyłu
+        for(int j=0; j<320; j++){
+            int x = (int)fgetc(file);
+            if(x>=prog){
+                video_memory[320*i + j] = x;
+            }
+            else{
+                video_memory[320*i + j] = 0;
+            }
+        }																
+    }	
+    ustawPaleteKolorow(kolor);
+    fclose(file);
+    getch();
+    trybTekstowy();
+}
+void negatyw(){
+    clrscr();
+    trybGraficzny();
+    FILE * file = fopen(fileName[fileNumber], "rb");
+    fread(&BM_fileHeader, sizeof(BITMAPFILEHEADER), 1, file);
+    fread(&BM_infoHeader, sizeof(BITMAPINFOHEADER), 1, file); 
 
-		fread(&BM_fileHeader, sizeof(BITMAPFILEHEADER), 1, file);
-		fread(&BM_infoHeader, sizeof(BITMAPINFOHEADER), 1, file); 
-		
+    fread(&paleta, sizeof(RGBQUAD),256,file);
 
-        //here ustawianie kolorków na czarno białe:
-		outportb(0x03C8, 0); 				//rozpocznij ustawianie palety od koloru nr 0
-		for (int i = 0; i < 256; i++) 		//ilość kolorów w palecie 8-bitowej
-		{
-            
-			 outp(0x03C9,   i * 63 / 255); 	//skalowana składowa R
-			 outp(0x03C9,   i * 63 / 255); 	//skalowana składowa G
-			 outp(0x03C9,   i * 63 / 255); 	//skalowana składowa B
-		}
+    int wartosc;
+    for(int i=199; i>=0; i--){											//obraz jest od tyłu
+        for(int j=0; j<320; j++){
+             video_memory[320*i + j] = ~fgetc(file); 	
+        }																
+    }	
+    ustawPaleteKolorow(kolor);
+    fclose(file);
+    getch();
+    trybTekstowy();
+}
 
-       
-        //here wyswietlanie obrazu:
-		fseek(file,BM_fileHeader.bfOffBits,SEEK_SET);  				// Od poczatku pliku przesuwa o ten offset dzieki czemu jesteśmy w miejscu gdzie zaczynaja sie konkretne piksele obrazu
-		
-		for(int k=199; k>=0; k--){											//Wczytywanie jest "od dolu", stad dekrementacja 
-			for(int j=0; j<320; j++){
-				 video_memory[320*k + j] = fgetc(file); 		//Ogolnie to dzialamy na calym obrazku jako na jednowymiarowej tablicy...
-			}																//Kazda linijka jest wczytywana co 320 znak
-		}																	//Stad to kazdego pojedynczego piksela zaczytujemy piksel obrazka
-		fclose(file);
-
-getch();
-
-
-
-//here zaczynaja sie robic kolorki bo mi sie nudziło
-
-		file = fopen("IMAGES/BABOON.bmp", "rb");
-		
-		fread(&BM_fileHeader, sizeof(BITMAPFILEHEADER), 1, file);
-		fread(&BM_infoHeader, sizeof(BITMAPINFOHEADER), 1, file); 
-
-        RGBQUAD paleta; //nagłówek nr 2 bitmapy
-
-        
-		
-		outportb(0x03C8, 0); 				//rozpocznij ustawianie palety od koloru nr 0
-		for (k = 0; k < 256; k++) 		//ilość kolorów w palecie 8-bitowej
-		{
-            fread(&paleta, sizeof(RGBQUAD),1,file);
-			outp(0x03C9,   (int)paleta.rgbRed * 63 / 255); 	//skalowana składowa R
-			outp(0x03C9,   (int)paleta.rgbGreen * 63 / 255); 	//skalowana składowa G
-			outp(0x03C9,   (int)paleta.rgbBlue * 63 / 255); 	//skalowana składowa B
-		}
-
-fclose(file);
-//here koniec kolorkow
-
-
-getch();
-
+int main(){
+    int opcja = 1;
+    while(opcja != 7){
+        clrscr();
+        cout<<"Wybierz:\n\
+        1.Negatyw\n\
+        2.Progowanie\n\
+        3.Wyswietl\n\
+        4.Kolor\n\
+        5.Czarno biale\n\
+        6.Zmien plik\n\
+        7.Zakoncz\n";
+        cin>>opcja;
+        switch(opcja){
+            case 1:
+                negatyw();
+            break;
+            case 2:
+                progowanie();
+            break;
+            case 3:
+                wyswietl();
+            break;
+            case 4:
+                kolor = 0;
+            break;
+            case 5:
+                kolor = 1;
+            break;
+            case 6:
+                wybierzPlik();
+            break;
+            case 7:
+                //exit
+            break;
+        }
+    }
 return 0;
-	}//int main
+}
